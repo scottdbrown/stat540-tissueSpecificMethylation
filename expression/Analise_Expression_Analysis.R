f74f49eb82c15ee55b741073c1d59cdf4e2eaf19
ahofmann4 <analise.h.1@gmail.com>
2014-03-31 14:55:17 -0700

    log2 transformed the data!

--- expression/Analise_Expression_Analysis.R
+++ expression/Analise_Expression_Analysis.R
@@ -12,15 +12,18 @@
 
 ####Libraries:
 library(preprocessCore)
+library(limma)
 
+#log2 transform the data:
+expDatlog  <- log2(expDat)
 
 #get dimensions, did visual check of column names in expDat, and row names in expMeta 
-dim(expDat) 
+dim(expDatlog) 
 dim(expMeta) 
 
 #check for NAs
-checkExpNAs  <-  is.na(expDat)
-omitExpNA  <- na.exclude(expDat)
+checkExpNAs  <-  is.na(expDatlog)
+omitExpNA  <- na.exclude(expDatlog)
 
 #check dimensions against expDat
 dim(omitExpNA)
@@ -32,11 +35,11 @@ dim(omitExpNA)
 ##############
 ##NORMALIZATION#######
 
-heatmap(cor(expDat), scale = "none", main="prenormalization Expression Correlation")
-heatmap(cor(expDat), scale = "none", Rowv = NA, Colv = NA, main="prenormalization Expression Correlation") #saved
+heatmap(cor(expDatlog), scale = "none", main="prenormalization Expression Correlation")
+heatmap(cor(expDatlog), scale = "none", Rowv = NA, Colv = NA, main="prenormalization Expression Correlation") #saved
 
 #Check for outliers: before normalizing quantiles
-corMatrix <- cor(expDat)
+corMatrix <- cor(expDatlog)
 meanCor <- data.frame(apply(corMatrix, 2, FUN = mean))
 meanCor <- t(meanCor)
 
@@ -47,43 +50,60 @@ orderMeanCor <- order(meanCor)
 orderedMeanCor <- data.frame(meanCor[, orderMeanCor])
 plot(orderedMeanCor[1:100, ], main = "prenormalized Expression Correlation") #saved
 
-
-#Check for outliers: after normalizing quantiles
-NormExpDat  <- data.frame(normalize.quantiles(as.matrix(expDat))) #need to give NormExpDat column names. 
+#Now that the data is log2 transformed it is harder to visually remove outliers and make a judgement where the cutoff is.. 
+#
+Check for outliers: after normalizing quantiles
+NormExpDat  <- data.frame(normalize.quantiles(as.matrix(expDatlog))) #need to give NormExpDat column names. 
 colnames(NormExpDat)  <- colnames(expDat)
 row.names(NormExpDat)  <- row.names(expDat)
 corMatrix <- cor(NormExpDat)
 meanCor <- data.frame(apply(corMatrix, 2, FUN = mean))
 meanCor <- t(meanCor)
-
-#colnames(meanCor) = colnames(transCombineData)
-# apply(meanCor, 1, FUN = min) #prints the minimum
-
+# 
+# #colnames(meanCor) = colnames(transCombineData)
+# # apply(meanCor, 1, FUN = min) #prints the minimum
+# 
 orderMeanCor <- order(meanCor)
 orderedMeanCor2 <- data.frame(meanCor[, orderMeanCor])
 plot(orderedMeanCor2[1:100, ], main = "post normalization Expression Correlation") #saved
 
-heatmap(cor(NormExpDat), scale = "none", Rowv = NA, Colv = NA, main="post normalization Expression Correlation") #saved
-heatmap(cor(NormExpDat), scale = "none", main="post normalization Expression Correlation") #saved
+# heatmap(cor(NormExpDat), scale = "none", Rowv = NA, Colv = NA, main="post normalization Expression Correlation") #saved
+# heatmap(cor(NormExpDat), scale = "none", main="post normalization Expression Correlation") #saved
+# 
+# #The three most apparent outliers ar GSM760199, GSM760185, and GSM60176. (The rest of the probes are within 70% correlation with the other samples)
+# #Look at outliers in context of meta data:
+# 
+# expMeta["GSM760176",] #Somatic Diaphragm Adult Female
+# expMeta["GSM760199",] #Somatic Skeletal Muscle Adult Male
+# expMeta["GSM760185",] #Somatic Skeletal Muscle Adult Female
+# 
+# #remove the outliers, and renormalize the data:
+# 
+# newExpDat  <- expDatlog
+# row.names(newExpDat)  <- row.names(expDat)
+# newExpDat$GSM760199  <- NULL
+# newExpDat$GSM760185  <- NULL
+# newExpDat$GSM760176  <- NULL
+# 
+# NormNew.ExpDat  <- normalize.quantiles(as.matrix(newExpDat))
+# row.names(NormNew.ExpDat)  <- row.names(newExpDat)
+# colnames(NormNew.ExpDat)  <- colnames(newExpDat)
+# 
+# heatmap(cor(NormNew.ExpDat), main = "PostOutlier, Norm Exp Data", scale = "none")
 
-#The three most apparent outliers ar GSM760199, GSM760185, and GSM60176. (The rest of the probes are within 70% correlation with the other samples)
-#Look at outliers in context of meta data:
+##NORMALIZATION DONE##
 
-expMeta["GSM760176",] #Somatic Diaphragm Adult Female
-expMeta["GSM760199",] #Somatic Skeletal Muscle Adult Male
-expMeta["GSM760185",] #Somatic Skeletal Muscle Adult Female
+###################################
+#Differential Expression Analysis##
+###################################
 
-#remove the outliers, and renormalize the data:
+#make model matrix
 
-newExpDat  <- expDat
-row.names(newExpDat)  <- row.names(expDat)
-newExpDat$GSM760199  <- NULL
-newExpDat$GSM760185  <- NULL
-newExpDat$GSM760176  <- NULL
+#remove rows from Meta data that correspond to our outliers that were removed. 
+##newMeta <- expMeta[-c(235,221,212),]  #don't need this anymore since the outliers are not taken out yet... 
 
-NormNew.ExpDat  <- normalize.quantiles(as.matrix(newExpDat))
-row.names(NormNew.ExpDat)  <- row.names(newExpDat)
-colnames(NormNew.ExpDat)  <- colnames(newExpDat)
+model.ExpDat  <- model.matrix(~characteristics_ch1.2,expMeta)
 
-heatmap(cor(NormNew.ExpDat), main = "PostOutlier, Norm Exp Data", scale = "none")
-
+ExpFit <- lmFit(NormExpDat, model.ExpDat)
+ExpEbFit <- eBayes(ExpFit)
+ExpTissue.Table <- topTable(ExpEbFit, number = Inf, coef = "characteristics_ch1.2cell type: Somatic.Tissue")
