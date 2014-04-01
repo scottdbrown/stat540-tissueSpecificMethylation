0a3fbfa83b34e7227ef73d589b962225c29e41f8
nzllim <nzllimhkust@gmail.com>
2014-03-31 18:46:42 -0700

    Minor HUGO Code Error.

--- expression/IlluminaGEO.R
+++ expression/IlluminaGEO.R
@@ -16,6 +16,7 @@ Illumina.GEO <- getGEO(filename = 'GPL6947.soft')
 # Annotation Dataframe Restructuring (for HUGO SYMBOLs only)
 Illumina.GEO.TRIM <- Illumina.GEO@dataTable@table
 Illumina.HUGO <- data.frame(ID = Illumina.GEO.TRIM$ID, HUGO = Illumina.GEO.TRIM$ILMN_Gene)
+rownames(Illumina.HUGO) <- Illumina.HUGO$ID
 
 # RData Repacking
 save(Illumina.HUGO, file = 'IlluminaHTMappingTable.HUGO')
